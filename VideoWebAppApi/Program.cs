e808fb7360d702c5c96117d3145c63f6bfe7cf54
Acensti <130999724+Sylkio@users.noreply.github.com>
2023-11-21 01:35:36 +0100

    Serilog + program setup

--- VideoWebAppApi/Program.cs
+++ VideoWebAppApi/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using VideoWebAppApi.Data; 
+using VideoWebAppApi.Interface;
+using VideoWebAppApi.Service; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register AzureService (You need to implement this according to your Azure Blob Storage setup)
+builder.Services.AddScoped<IAzureService, AzureService>();
+
+// Register AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -23,3 +33,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+try
+{
+    app.Run();
+    Log.Information("API is now ready to serve files to and from Azure Cloud Storage...");
+}
+catch (Exception ex) when (!ex.GetType().Name.Equals("StopTheHostException", StringComparison.Ordinal))
+{
+    Log.Fatal(ex, "Unhandled Exception");
+}
+finally
+{
+    Log.Information("Azure Storage API Shutting Down...");
+    Log.CloseAndFlush();
+}
